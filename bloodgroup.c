e250f882b1301a8d16b7d67780ba52d4f468d7af
NJT123456 <jinjutha.njt@mail.kmutt.ac.th>
2021-09-16 23:11:22 +0700

    Modified bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -39,9 +39,9 @@ void main()
     else
     {
       printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
-      printf("  But Sad! You can receive blood only from: O\n");
+      printf("  But Good! You can receive blood only from: O\n");
     }
-    printf("\nContinue (YES for Yes)? ");
+    printf("\nContinue (YES or NO)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
   printf("Goodbye\n");
